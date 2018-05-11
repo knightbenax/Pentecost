d6056458d2ecf9555fb7c362285c1274e30b1d1e
Bezaleel Ashefor <knightbenax@gmail.com>
2018-05-11 01:25:02 +0100

    Added Loader

--- app/src/main/java/app/ephod/pentecost/sample/MainActivity.java
+++ app/src/main/java/app/ephod/pentecost/sample/MainActivity.java
@@ -2,17 +2,30 @@ package app.ephod.pentecost.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import app.ephod.pentecost.library.paystack.PaymentView;
 
 public class MainActivity extends AppCompatActivity {
 
     PaymentView paymentView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        paymentView = findViewById(R.id.paymentView);
+
+        button = paymentView.getPayButton();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                paymentView.showLoader();
+            }
+        });
     }
 }
